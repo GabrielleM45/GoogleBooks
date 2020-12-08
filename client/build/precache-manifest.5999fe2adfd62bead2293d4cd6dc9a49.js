b4a3f14d603b276dad1506fa900668ca744f1b74
gbluemd <gsjujub@gmail.com>
2020-12-08 16:04:11 -0700

    deploy

---
+++ client/build/precache-manifest.5999fe2adfd62bead2293d4cd6dc9a49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f324139cf1f885b252b4",
+    "url": "/static/css/main.63aee320.chunk.css"
+  },
+  {
+    "revision": "f324139cf1f885b252b4",
+    "url": "/static/js/main.cb20845e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4fe38a36e2bf36575327",
+    "url": "/static/js/2.04ca804c.chunk.js"
+  },
+  {
+    "revision": "163fd12151fa5c8c836df223259b9e52",
+    "url": "/static/media/shelf.163fd121.mp4"
+  },
+  {
+    "revision": "4a1c6747ed8f1504a1e038d7a0ae3442",
+    "url": "/index.html"
+  }
+];
